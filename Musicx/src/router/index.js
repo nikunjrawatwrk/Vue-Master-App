0fbbe705cf9b6cb31b6fdd7eb3f253596db1d6ed
nikunj.rawat <nikunj.rawat@sunarctechnologies.com>
2023-02-07 15:08:07 +0530

    Named Routes creates for Homepage link

--- Musicx/src/router/index.js
+++ Musicx/src/router/index.js
@@ -1,25 +1,28 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
 import Manage from "@/views/Manage.vue";
 
 const routes = [
   {
+    name: "home",
     path: "/",
     component: Home,
   },
   {
+    name: "about",
     path: "/about",
     component: About,
   },
   {
+    name: "manage",
     path: "/manage",
     component: Manage,
   },
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   linkExactActiveClass: "text-yellow-500",
 });
